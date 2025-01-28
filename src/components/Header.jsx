f9351cad277136ecff74f4a0fd8ae4b47cf7ec94
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-01-28 10:53:08 +0100

    feat: add base structure to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,24 @@ const Header = () => {
     <header>
       <div className="d-flex justify-content-between m-3">
         <div className="logo">LOGO</div>
-        <div className="search">SEARCH</div>
+        <div className="search">
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Cerca..."
+              aria-label="Example text with button addon"
+              aria-describedby="button-addon1"
+            />
+            <button
+              className="btn btn-outline-secondary"
+              type="button"
+              id="button-addon1"
+            >
+              CERCA
+            </button>
+          </div>
+        </div>
       </div>
     </header>
   );
