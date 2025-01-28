5b05a6152a6ec5939f0ecd4c39fdfc52037ccf85
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-01-28 10:49:40 +0100

    feat: add header and main components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+};
+
+export default App;
+
+// https://api.themoviedb.org/3/search/movie?api_key=253ed4ba34df710d4265508f7eccdabc
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="d-flex justify-content-between m-3">
+        <div className="logo">LOGO</div>
+        <div className="search">SEARCH</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
